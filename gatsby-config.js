ebb88bd0f722c1cdd7af8d33a9291a6c20bbac7b
Harjot Singh <harjot@harkul.com>
2020-06-16 19:10:35 +0100

    refactor: remove default content and move keep pages and content separate

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        path: `${__dirname}/src/content`,
+        name: 'content',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         path: `${__dirname}/src/img`,
         name: 'images',
       },
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Layout from '../../components/Layout'
-import BlogRoll from '../../components/BlogRoll'
+import Layout from '../components/Layout'
+import BlogRoll from '../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
