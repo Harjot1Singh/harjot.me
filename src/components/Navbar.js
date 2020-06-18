e6cf6056f15e8f2914e2026e562fa7abe6d34262
Harjot Singh <harjot@harkul.com>
2020-06-18 22:18:03 +0100

    feat: add navbar with styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,60 +1,70 @@
-import React, { useState } from 'react'
-import { Link } from 'gatsby'
-import clsx from 'clsx'
+import React, { useEffect } from 'react'
+import { Link as GatsbyLink } from 'gatsby'
+import { Link } from 'react-scroll'
+import { createUseStyles } from 'react-jss'
+import useScrollPosition from '@react-hook/window-scroll'
+import { animated, useSpring } from 'react-spring'
+
+const useStyles = createUseStyles( ( { color, font } ) => ( {
+  navbar: {
+    position: 'fixed',
+    top: '10px',
+    right: '20px',
+    display: 'flex',
+    fontSize: '16px',
+    borderRadius: '100px',
+  },
+  item: {
+    padding: '20px 50px',
+    textTransform: 'uppercase',
+    fontSize: '32px',
+    color: color.primary,
+    fontFamily: font.header,
+    textDecoration: 'none',
+    '&:hover': {
+      color: color.secondary,
+    },
+  },
+  active: {
+    fontWeight: 'bold',
+  },
+} ) )
+
+const poppedInSpringConfig = { background: 'rgba(0, 0, 0, 0)' }
+const floatingSpringConfig = { background: 'rgba(94, 93, 98, 0.9)' }
+
+const items = [
+  [ 'Home', ( props ) => <Link {...props} to="home" href="#home" /> ],
+  [ 'About', Link ],
+  [ 'Projects', Link ],
+  [ 'Contact', Link ],
+  [ 'Blog', GatsbyLink ],
+]
 
 const Navbar = () => {
-  const [ active, setActive ] = useState( false )
-  const [ navbarClass, setNavbarClass ] = useState( '' )
+  const threshold = useScrollPosition() > 2
 
-  const toggleHamburger = () => {
-    setActive( !active )
-    setNavbarClass( !active ? 'is-active' : '' )
-  }
+  const [ props, setProps ] = useSpring( () => poppedInSpringConfig )
+
+  useEffect( () => {
+    setProps( threshold ? floatingSpringConfig : poppedInSpringConfig )
+  }, [ threshold, setProps ] )
+
+  const classes = useStyles( {} )
 
   return (
-    <nav
-      className="navbar is-transparent"
+    <animated.nav
+      style={props}
+      className={classes.navbar}
       role="navigation"
       aria-label="main-navigation"
     >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" title="Logo" />
-          {/* Hamburger menu */}
-          <div
-            className={clsx( 'navbar-burger burger', navbarClass )}
-            data-target="navMenu"
-            onClick={toggleHamburger}
-          >
-            <span />
-            <span />
-            <span />
-          </div>
-        </div>
-        <div
-          id="navMenu"
-          className={`navbar-menu ${navbarClass}`}
-        >
-          <div className="navbar-start has-text-centered">
-            <Link className="navbar-item" to="/about">
-              About
-            </Link>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-            <Link className="navbar-item" to="/blog">
-              Blog
-            </Link>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-          </div>
-        </div>
-      </div>
-    </nav>
+      {items.map( ( [ name, Link ] ) => (
+        <Link className={classes.item}>
+          {name}
+        </Link>
+      ) )}
+    </animated.nav>
   )
 }
 
