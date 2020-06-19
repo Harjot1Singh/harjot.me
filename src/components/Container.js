7fc47a1526ecb68f49d2671703182f8049c0fc8d
Harjot Singh <harjot@harkul.com>
2020-06-19 03:00:07 +0100

    feat: add Container component

---
+++ src/components/Container.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { node, string } from 'prop-types'
+import { createUseStyles } from 'react-jss'
+import clsx from 'clsx'
+
+const useStyles = createUseStyles( {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    maxWidth: '70vw',
+    alignItems: 'center',
+    margin: 'auto',
+  },
+} )
+
+const Container = ( { className, children } ) => {
+  const classes = useStyles()
+
+  return <div className={clsx( classes.root, className )}>{children}</div>
+}
+
+Container.propTypes = {
+  className: string,
+  children: node.isRequired,
+}
+
+Container.defaultProps = {
+  className: null,
+}
+
+export default Container
